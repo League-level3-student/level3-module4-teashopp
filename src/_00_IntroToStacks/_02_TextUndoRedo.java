646a9a42a8429f287ee19229bc0251e98c1143ec
ethan <ethan.yuan@gmail.com>
2022-10-23 11:25:31 -0700

    23 / 10 / 2022

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,16 +1,14 @@
 package _00_IntroToStacks;
 
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class _02_TextUndoRedo {
-
+	
 	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -29,7 +27,6 @@ public class _02_TextUndoRedo {
 	static JFrame frame = new JFrame();
 	static JPanel panel = new JPanel();
 	static JLabel label = new JLabel("");
-	static JTextField tf = new JTextField();
 
 	static Stack<String> edit = new Stack<String>();
 
@@ -40,86 +37,38 @@ public class _02_TextUndoRedo {
 
 		frame.setVisible(true);
 		frame.pack();
-
-		label.add(tf);
-
-		tf.addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
-
-				int key = e.getKeyCode();
-				String conv0 = Integer.toString(key);
-
-				int lastkey = e.getKeyCode();
-				String conv1 = Integer.toString(lastkey);
-
-				if (key == KeyEvent.VK_DELETE) {
-
-					edit.push(conv0);
-
-				}
-
-				else if (key == KeyEvent.VK_ENTER) {
-
-					edit.pop();
-					tf.setText(tf.getText() + conv1);
-
-				}
-
-				else {
-
-					tf.setText(tf.getText() + conv0);
-
-				}
-
-				System.out.println(conv0);
-			}
-		});
-
+		
 	}
 
-	/*
-	 * public void keyPressed(KeyEvent e) {
-	 * 
-	 * int key = e.getKeyCode(); String conv0 = Integer.toString(key);
-	 * 
-	 * int lastkey = e.getKeyCode(); String conv1 = Integer.toString(lastkey);
-	 * 
-	 * if (key == KeyEvent.VK_DELETE) {
-	 * 
-	 * edit.push(conv0);
-	 * 
-	 * }
-	 * 
-	 * else if (key == KeyEvent.VK_ENTER) {
-	 * 
-	 * edit.pop(); label.setText(label.getText() + conv1);
-	 * 
-	 * }
-	 * 
-	 * else {
-	 * 
-	 * label.setText(label.getText() + conv0);
-	 * 
-	 * }
-	 * 
-	 * System.out.println(conv0);
-	 * 
-	 * }
-	 */
+	public void keyPressed(KeyEvent e) {
+
+		int key = e.getKeyCode();
+		String conv0 = Integer.toString(key);
+
+		int lastkey = e.getKeyCode();
+		String conv1 = Integer.toString(lastkey);
+
+		if (key == KeyEvent.VK_DELETE) {
+
+			edit.push(conv0);
+
+		}
+
+		else if (key == KeyEvent.VK_ENTER) {
+
+			edit.pop();
+			label.setText(label.getText() + conv1);
+
+		}
+
+		else {
+
+			label.setText(label.getText() + conv0);
+
+		}
+		
+		System.out.println(conv0);
+
+	}
 
 }
