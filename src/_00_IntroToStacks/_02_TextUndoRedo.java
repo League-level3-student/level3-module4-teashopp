126ebdf260186603777d853801e05b3b40e3f689
ethan <ethan.yuan@gmail.com>
2022-11-13 11:28:00 -0800

    13 / 11 / 22

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -9,7 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class _02_TextUndoRedo implements KeyListener {
-	
+
 	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -29,15 +29,17 @@ public class _02_TextUndoRedo implements KeyListener {
 	static JPanel panel = new JPanel();
 	static JLabel label = new JLabel();
 
+	Stack<Character> deleted = new Stack<Character>();
+
 	public static void main(String[] args) {
-		
+
 		_02_TextUndoRedo tur = new _02_TextUndoRedo();
 		tur.setup();
-		
+
 	}
-	
+
 	public void setup() {
-		
+
 		frame.add(panel);
 		panel.add(label);
 		frame.addKeyListener(this);
@@ -46,64 +48,52 @@ public class _02_TextUndoRedo implements KeyListener {
 		frame.pack();
 
 	}
-	
-	
-	public void keyPressed(KeyEvent e) {
-		
-		Stack<String> text = new Stack<String>();
-		Stack<String> pop = new Stack<String>();
-		
-		char key = e.getKeyChar();
-		String stringkey = String.valueOf(key);
-		
-		int last = e.getKeyCode() - 1;
-		String stringlast = String.valueOf(last);
 
-		if( key == KeyEvent.VK_BACK_SPACE) {			
-			
-			// remove from jlabel
-			text.pop();
-			
-			// save to stack
-			pop.push(stringkey);
-	
-			// update text
-			label.setText(label.getText());
-			
+	public void keyPressed(KeyEvent e) {
+
+		char keychar = e.getKeyChar();
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_BACK_SPACE) {
+
+			if (label.getText().length() > 0) {
+				
+				deleted.push(label.getText().charAt(label.getText().length() - 1));
+				label.setText(label.getText().substring(0, label.getText().length() - 1));
+							
+			}
+
 		}
 
-		else if(key == KeyEvent.VK_ENTER) {
-			
-			pop.pop();
-			text.push(stringlast);			
-			label.setText(label.getText() + text.toString());
+		else if (key == KeyEvent.VK_ENTER) {
 
+			if (deleted.size() > 0) {
+				
+				label.setText(label.getText() + deleted.pop());
+
+			}
 		}
 
 		else {
-			
-			text.push(stringkey);
-			label.setText(label.getText() + text.toString());
-			
+
+			label.setText(label.getText() + keychar);
+
 		}
-		
-		System.out.println(text.toString());
-		
+
+		System.out.println(label.getText());
+
 	}
 
-	
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
 }
