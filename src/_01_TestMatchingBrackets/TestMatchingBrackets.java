e07e32ff222b700b8babee3a40467b7d24c617a4
ethan <ethan.yuan@gmail.com>
2022-11-20 11:28:30 -0800

    20 / 11 / 2022

--- src/_01_TestMatchingBrackets/TestMatchingBrackets.java
+++ src/_01_TestMatchingBrackets/TestMatchingBrackets.java
@@ -1,12 +1,62 @@
 package _01_TestMatchingBrackets;
 
-public class TestMatchingBrackets {
-    /*
-     * Use a Stack to complete the method for checking if every opening bracket
-     * has a matching closing bracket
-     */
-    public static boolean doBracketsMatch(String b) {
+import java.util.Stack;
 
-        return false;
-    }
+public class TestMatchingBrackets {
+	/*
+	 * Use a Stack to complete the method for checking if every opening bracket has
+	 * a matching closing bracket
+	 */
+	public static boolean doBracketsMatch(String b) {
+
+		Stack<Character> check = new Stack<Character>();
+
+		// add {} to stack
+		for (int i = b.length() - 1; i >= 0; i--) {
+			check.push(b.charAt(i));
+		}
+		
+		System.out.println(check);
+		
+		int total = 0;
+
+		// even total
+		if (check.size() % 2 == 0) {
+
+			while (check.size() > 0) {
+				
+				char poppedchar = check.pop();
+				System.out.println(poppedchar);
+				
+				if(poppedchar == '{') {
+					total = total + 1;
+				}
+				
+				else {
+					total = total - 1;
+				}
+				
+				if(total < 0) {
+					return false;
+				}
+				
+			}
+			
+			
+			if(total == 0) {
+				return true;
+			}
+			
+			else {
+				return false;
+			}
+		
+		}
+
+		// odd total
+		else {
+			return false;
+		}
+		
+	}
 }
